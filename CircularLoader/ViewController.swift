0b4534dcdeb8fdb1226f0f596f8b7aa0687e5704
ElijahButers <Elijahbuters@gmail.com>
2017-12-15 20:40:44 +0200

    Add code to viewDidLoad() method to ViewController class that creates a circular shape in the center of the view.

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let shapeLayer = CAShapeLayer()
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        view.layer.addSublayer(shapeLayer)
     }
 
     override func didReceiveMemoryWarning() {
