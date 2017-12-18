13687c76fa0521723a2c0e40ff3e696b805378ed
ElijahButers <Elijahbuters@gmail.com>
2017-12-18 16:58:29 +0200

    Fixed the Info.plist issue.

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         let center = view.center
         let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         shapeLayer.path = circularPath.cgPath
+        
         view.layer.addSublayer(shapeLayer)
     }
 
