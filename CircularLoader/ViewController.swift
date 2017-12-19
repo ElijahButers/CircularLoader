73465690ad5c2637436c2553b0dce20efcf57a2b
ElijahButers <Elijahbuters@gmail.com>
2017-12-19 16:48:06 +0200

    Add code to viewDidLoad() method in ViewController class that creates a track layer on the circle.

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -20,9 +20,16 @@ class ViewController: UIViewController {
         let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: CGFloat(-CFloat.pi / 2), endAngle: 2 * CGFloat.pi, clockwise: true)
         shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineWidth = 18
+        shapeLayer.lineWidth = 10
         shapeLayer.strokeEnd = 0
         view.layer.addSublayer(shapeLayer)
+        
+        let trackLayer = CAShapeLayer()
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.clear.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.strokeEnd = 0
+        view.layer.addSublayer(trackLayer)
         
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
