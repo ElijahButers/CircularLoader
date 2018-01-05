96d6e6d403225c4c3b195d3c59e93296e4786c5e
ElijahButers <Elijahbuters@gmail.com>
2018-01-05 16:18:41 +0200

    Add a new swift extension class to the project.

---
+++ CircularLoader/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  CirculaeLoader
+//
+//  Created by User on 1/5/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+}
